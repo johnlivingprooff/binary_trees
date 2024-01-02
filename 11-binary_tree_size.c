79eed47d861da115140bca293917d0635e2081f3
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-03 00:57:23 +0200

    updating tree size codestyle

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,13 +7,13 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t left, right;
-    
-    if (!tree)
-        return (0);
+	size_t left, right;
 
-    left = binary_tree_size(tree->left);
-    right = binary_tree_size(tree->right);
+	if (!tree)
+		return (0);
 
-    return (1 + right + left);
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+
+	return (1 + right + left);
 }
