d29d9150f6175f6f6a166e1233fa40d2eebe799c
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-02 22:00:17 +0200

    node is root/leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,5 +7,11 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (!node)
+		return (0);
 
+	if (node->left || node->right)
+		return (0);
+
+	return (1);
 }
---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if node is the root
+ * @node: node to be checked
+ * Return: 1 if node is a root, 0 if node is null or not root
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (!(node->left) || !(node->right) || node->parent)
+		return (0);
+
+	return (1);
+}
