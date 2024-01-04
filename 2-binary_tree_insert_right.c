bb10f2cf2de54c9199a2f8272c5697c4d51039ca
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-04 13:36:23 +0200

    revert

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,9 +8,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *right_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *right_node;
 
-	if (parent == NULL || !right_node)
+	if (!parent)
+		return (NULL);
+
+	right_node = malloc(sizeof(binary_tree_t));
+	if (!right_node)
 		return (NULL);
 
 	right_node->n = value;
@@ -24,6 +28,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 
 	parent->right = right_node;
+
 
 	return (right_node);
 }
