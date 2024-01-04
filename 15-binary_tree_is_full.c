82aec06156ee3e4af67d95b72109e84f92ae7b70
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-04 10:02:12 +0200

    updating tree-is-full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,14 +7,14 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    if ((tree->left && !tree->right) || (tree->right && !tree->left))
-        return (0);
+	if ((tree->left && !tree->right) || (tree->right && !tree->left))
+		return (0);
 
-    if (!(tree->left) && !(tree->right))
-        return (1);
+	if (!(tree->left) && !(tree->right))
+		return (1);
 
-    return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
