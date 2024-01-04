dfd55cac456fd3ef2a765efb48e3454067a5fdbc
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-04 14:30:36 +0200

    codestyle

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,7 +10,7 @@ int tree_height(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left_height = tree_height(tree->left);
 	right_height = tree_height(tree->right);
@@ -28,7 +28,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left_height = tree_height(tree->left);
 	right_height = tree_height(tree->right);
