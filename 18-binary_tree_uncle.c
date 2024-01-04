518a3fbd975eb054dfdd05bcf4d6be6aecc5ed42
johnlivingprooff <johnlivingprooff@gmail.com>
2024-01-04 10:20:34 +0200

    found Uncle node*

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle node of node
+ * @node: pointer to the query node
+ * Return: pointer to the uncle node
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !(node->parent))
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+	{
+		if (node->parent->parent->right)
+			return (node->parent->parent->right);
+	}
+
+	if (node->parent == node->parent->parent->right)
+	{
+		if (node->parent->parent->left)
+			return (node->parent->parent->left);
+	}
+
+	return (NULL);
+}
